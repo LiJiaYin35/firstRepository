a53fffb44f786aeb80b2a6b91a65fc3611f2fb18
lijiayin <lijiayin35@126.com>
2019-05-18 12:05:14 +0800

    第一次提交

---
+++ hello.py
@@ -0,0 +1,7 @@
+
+print 'hello1'
+Administrator@PC-201711261723 MINGW64 /
+$ ls
+bin/  etc/           LICENSE.txt  ReleaseNotes.html  unins000.dat   usr/
+cmd/  git-bash.exe*  mingw64/     test/              unins000.exe*
+dev/  git-cmd.exe*   proc/        tmp/               unins000.msg
