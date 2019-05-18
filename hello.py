8f0b67b4950d10a71e815d0f197b378d87bd9381
lijiayin <lijiayin35@126.com>
2019-05-18 12:46:13 +0800

    dev分支中修改hello.py文件

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
 
 print('hello1')
 print('修改文件hello2')
+
+print('dev分支中添加hello3')
 Administrator@PC-201711261723 MINGW64 /
 $ ls
 bin/  etc/           LICENSE.txt  ReleaseNotes.html  unins000.dat   usr/
